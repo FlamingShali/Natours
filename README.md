f1cfe67f939af138a1b894e3af4a264172f61acb
Michał <michal.klepner1@o2.pl>
2024-08-24 11:46:35 +0200

    refactored for MVC

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 It's my first node.js small project with using mongoDB created threw the course of node.js.
 
+
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
@@ -12,36 +13,13 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    userCreateIndex: true,
-    useFindAndModify: false
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
-  .then(() => {
-    console.log('DB connection seccessful!');
-  });
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const app = require('./app');
+  .then(con => console.log('DB connection successful!!!!!!!!!!!!!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
-Testowy commit
