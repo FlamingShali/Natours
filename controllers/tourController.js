3031e212562a77c491f562e33a9d3aea0666e6ae
Michał <michal.klepner1@o2.pl>
2024-08-26 11:33:59 +0200

    pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,6 +27,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    //page=2&limit=10 1-10, page 1, 11-20, page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     const tours = await query;
     // {difficulty: 'easy', duration: { $gte: 5 }}
 
