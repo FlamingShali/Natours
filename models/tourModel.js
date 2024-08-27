55d7e237f0cf973dddc88d7af6e68757a49ed446
Michał <michal.klepner1@o2.pl>
2024-08-27 15:02:02 +0200

    first validators added

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,3 +1,4 @@
+const { kMaxLength } = require('buffer');
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
@@ -6,7 +7,10 @@ const tourSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
-      unique: true
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters']
     },
     slug: String,
     duration: {
@@ -19,11 +23,17 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have difficulty']
+      required: [true, 'A tour must have difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be less or equal to 5.0']
     },
     ratingsQuantity: {
       type: Number,
